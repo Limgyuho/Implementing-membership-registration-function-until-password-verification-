e3da7b8ab8d6e605bd9a97bb153f949e5ac59440
Limgyuho <radum0719@hanmail.net>
2022-12-28 23:45:37 +0900

    회원가입 기능 구현중(비밀번호 확인 까지)

---
+++ src/creatboard9/Main.java
@@ -0,0 +1,10 @@
+package creatboard9;
+
+import creatboard9.App;
+
+public class Main {
+	public static void main(String[] args) {
+		new App().run();
+	}
+}
+
---
+++ src/creatboard9/dto/Dto.java
@@ -0,0 +1,7 @@
+package creatboard9.dto;
+
+public class Dto {
+	public int id;
+	public String regDate;
+
+}
